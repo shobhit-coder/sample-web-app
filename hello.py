1c5e3bfbc353af9333e3755a76d38f31d9341f9a
shobhit-coder <shobhit1998@yahoo.co.in>
2019-08-14 23:56:50 +0530

    added file

---
+++ hello.py
@@ -0,0 +1,9 @@
+from flask import Flask
+myapp = Flask(__name__)
+
+@myapp.route("/")
+def hello():
+    return "Hello Flask, on Azure App Service for Linux"
+
+# if __name__ == "__main__":
+#     myapp.run(debug=True)
