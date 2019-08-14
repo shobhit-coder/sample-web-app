0534c2fe98c991a6b81d4396d46c1a244bea2237
shobhit-coder <shobhit1998@yahoo.co.in>
2019-08-15 00:57:21 +0530

    added new page

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 myapp = Flask(__name__)
 
 @myapp.route("/")
 def hello():
     return "Hello Flask, on Azure App Service for Linux"
 
+@myapp.route("/a")
+def aa():
+    return "12123Hello Flask, on Azure App Service for Linux"
+
+@myapp.route("/shobhit")
+def shobhit():
+    return render_template("home.html")
 # if __name__ == "__main__":
 #     myapp.run(debug=True)
